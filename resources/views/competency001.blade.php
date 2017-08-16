2f22619b85b704c6cccb19f3b27623479a0231c4
Ram Ochoa <ram.ochoa.hes@gmail.com>
2017-08-16 11:48:06 -0500

    add Google analytics script

--- resources/views/competency001.blade.php
+++ resources/views/competency001.blade.php
@@ -10,6 +10,7 @@
     <div class="panel-body">
 
         <!-- Nav tabs -->
+
         <ul class="nav nav-tabs" role="tablist">
           <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Text</a></li>
           <li role="presentation"><a href="#questions" aria-controls="questions" role="tab" data-toggle="tab">Quiz</a></li>
